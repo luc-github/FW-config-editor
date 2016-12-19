fa68fbb461822cdb8accbf579e6039d1fba43225
Luc <luc.lebosse@gmail.com>
2016-12-19 12:28:41 +0100

    Fix glup version to 4
    fix compilation issues

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,21 @@ var gulp = require('gulp'),
 function clean() {
     return del(['dist']);
 }
+   
+function minifyHTML() {
+        return gulp.src('app/config.html')
+        .pipe(gulp.dest('dist'))
+}
+
+function scripts() {
+        return gulp.src('app/js/scripts.js')
+        .pipe(gulp.dest('dist/js'))
+}
+
+function style() {
+        return gulp.src('app/css/style.css')
+        .pipe(gulp.dest('dist/css'))
+}
 
 function smoosh() {
     return gulp.src('dist/config.html')
@@ -19,11 +34,15 @@ function compress() {
         .pipe(gulp.dest('.'));
 }
 
-gulp.task(clean);
-gulp.task(smoosh);
+gulp.task('clean', clean);
+gulp.task('minifyHTML', minifyHTML);
+gulp.task('scripts', scripts);
+gulp.task('style', style);
+gulp.task('smoosh', smoosh);
+gulp.task('compress', compress);
 
-var defaultSeries = gulp.series(clean,  smoosh);
-var packageSeries = gulp.series(clean,  smoosh, compress);
+var defaultSeries = gulp.series(clean, minifyHTML, scripts, style, smoosh);
+var packageSeries = gulp.series(clean, minifyHTML, scripts, style, smoosh, compress);
 
 gulp.task('default', defaultSeries);
 gulp.task('package', packageSeries);
