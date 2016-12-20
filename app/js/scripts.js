12b4fb9d2e9f91b5704118fefecbaeb5a932687d
luc lebosse <luc.lebosse@tech-hunters.com>
2016-12-20 22:00:26 +0100

    repetier filter implemented

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,24 +1,98 @@
+var target_FW = "repetier";
+
 function get_label(sline){
-    return "label";
+     if (target_FW.toLowerCase() == "smoothieware"){
+        return "label";
+    } else {
+        var tline =  sline.split(" ");
+        var tsize = tline.length;
+        if (tsize >3){
+            var result = "";
+            for (i = 3; i < tsize ; i++){
+                if (tline[i][0] == '[') break;
+                result += tline[i]+" ";
+            }
+            return result;
+            }
+        else  return "???";
+    }
 }
 
 function get_value(sline){
-    return "value";
+    if (target_FW.toLowerCase() == "smoothieware"){
+        return "value";
+    } else {
+        var tline =  sline.split(" ");
+        if ( tline.length >3){
+              return tline[2];
+        } else return "???";
+    }
 }
 
 function get_help(sline){
-    return "help";
+    if (target_FW.toLowerCase() == "smoothieware"){
+        return "help";
+    } else {
+         var tline =  sline.split("[");
+         if (tline.length > 1){
+            var tline2 =  tline[1].split("]");
+            return tline2[0];
+        } else  return "";
+    }
 }
 
 function get_command(sline){
-    return "set ";
+     if (target_FW.toLowerCase() == "smoothieware"){
+        return "set";
+    } else {
+         var tline =  sline.split(" ");
+        if ( tline.length >3){
+            var stype = tline[0].split(":");
+            var command = "M206 T" + stype[1];
+             command += " P" + tline[1];
+             if (stype[1] == "3") command += " X";
+             else command += " S";
+        return command;
+        } else return "; ";
+    }
+}
+
+function is_commented(sline){
+    if (target_FW.toLowerCase() == "smoothieware"){
+        line = sline.trim();
+        return  sline.startsWith("#");
+    } else {
+        return false;
+    }
+}
+
+function is_entry(sline){
+    if (target_FW.toLowerCase() == "smoothieware"){
+        if (sline.trim().length > 1)return true;
+        else return false;
+    } else {
+        return sline.startsWith("EPR:");
+    }
 }
 
 function create_entry(sentry){
-    var container_block = document.getElementById("editor_table");
-    //entry
-    var trblock = document.createElement("tr");
-    var tdblock = document.createElement("td");
+    var container_block = document.getElementById("editor_table");    
+    var trblock;
+     var tdblock;
+     if (!is_entry(sentry))return;
+     trblock = document.createElement("tr");
+     //comment
+    tdblock = document.createElement("td");
+     trblock.appendChild(tdblock);
+     //check if it is a smoothieware
+     if (target_FW.toLowerCase() != "smoothieware"){
+          var comment = document.getElementById("comment");
+          comment.style.display="none";
+          tdblock.style.display="none";
+     }
+   
+     //label
+    tdblock = document.createElement("td");
     tdblock.style="width:20px";
     var t = document.createTextNode(get_label(sentry)); 
     tdblock.appendChild(t);
@@ -28,7 +102,12 @@ function create_entry(sentry){
     tdblock = document.createElement("td");
     tdblock.style="width:20px";
     var inputblock = document.createElement("input");
-    inputblock.type="text";
+    if (target_FW.toLowerCase() != "smoothieware"){
+        inputblock.type="number";
+        inputblock.min=0;
+    } else {
+                inputblock.type="text";
+                }
     inputblock.style="width:70px";    
     inputblock.value = get_value(sentry);
     tdblock.appendChild(inputblock);
@@ -36,7 +115,13 @@ function create_entry(sentry){
     var buttonblock = document.createElement("button");
     t = document.createTextNode("Set");
     buttonblock.onclick = function() { 
-            alert(get_command(sentry) + inputblock.value); 
+            var value = inputblock.value;
+            if ((value.trim()[0]=='-') || (value=="") || (value.toLowerCase().indexOf("e")!=-1)){
+                inputblock.value=get_value(sentry);
+            } else {
+                alert(get_command(sentry) + value.trim()); 
+            }
+            
         };
     buttonblock.appendChild(t);
     tdblock.appendChild(buttonblock);
@@ -49,14 +134,28 @@ function create_entry(sentry){
 }
 
 window.onload = function() {
-    var sline = "EPR:2 83 360000 Stop stepper after inactivity [ms,0=off]";
-    create_entry(sline);
-    sline = "EPR:0 1028 7 Language";
-    create_entry(sline);
-    sline = "EPR:3 1160 180.000 Temp Ext PLA:";
-    create_entry(sline);
-    sline = "EPR:1 239 1 Extr.1 temp. stabilize time [s]";
-    create_entry(sline);
+    var sline;
+    if (target_FW.toLowerCase() == "repetier"){
+        document.getElementById("target_fw").innerHTML="Repetier EEPROM editor";
+        sline = "EPR:2 83 360000 Stop stepper after inactivity [ms,0=off]";
+        create_entry(sline);
+        sline = "EPR:0 1028 7 Language";
+        create_entry(sline);
+        sline = "EPR:3 1160 180.000 Temp Ext PLA:";
+        create_entry(sline);
+        sline = "EPR:1 239 1 Extr.1 temp. stabilize time [s]";
+        create_entry(sline);}
+    else if (target_FW.toLowerCase() == "smoothieware"){
+         document.getElementById("target_fw").innerHTML="Smoothieware config editor";
+        sline = "# Robot module configurations : general handling of movement G-codes and slicing into moves";
+        create_entry(sline);
+        sline = "default_feed_rate                            4000             # Default rate ( mm/minute ) for G1/G2/G3 moves";
+        create_entry(sline);
+        sline = "                                                              # note it is invalid for both the above be 0";
+        create_entry(sline);
+        sline = "##panel.contrast                               18                # override contrast setting (default is 18)";
+        create_entry(sline);}
+    else alert(target_FW.toLowerCase());
 };
 
 function Sendcommand(commandtxt, showresult){
